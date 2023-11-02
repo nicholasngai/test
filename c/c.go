9eb3c41c7314241a2077eb3b80e22697f45264d2
Nicholas Ngai <nicholas@ngai.me>
2023-11-01 19:42:38 -0700

    Add c

---
+++ c/c.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nicholasngai/test/a"
+	"github.com/nicholasngai/test/b"
+)
+
+func main() {
+	fmt.Println(a.Foo())
+	fmt.Println(b.Bar())
+}
---
+++ c/go.mod
@@ -0,0 +1,8 @@
+module github.com/nicholasngai/test/c
+
+go 1.21.1
+
+require (
+	github.com/nicholasngai/test/a v0.1.0
+	github.com/nicholasngai/test/b v0.1.0
+)
